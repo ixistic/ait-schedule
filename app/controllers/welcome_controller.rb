73e13388dd4f9beb6acaf773b3347e47450c4a91
ixistic <teeravipark@gmail.com>
2016-08-22 12:34:53 +0700

    random numeber 0,1 for test divide by 0

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-    @a = 1/0
-    Rails.logger.info "About to divide by 0"
+    x = rand(0..1)
+    if(x == 0)
+      @a = "About to divide by 0"
+    else
+      @a = 1/x
+    end
   end
 end
